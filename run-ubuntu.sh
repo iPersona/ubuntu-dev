5240732692a9a1aff52e8afed3255a0b145899de
iPersona <freedom2028@gmail.com>
2017-06-19 22:35:45 +0800

    Add: mount different shared folder by system type

--- run-ubuntu.sh
+++ run-ubuntu.sh
@@ -1,1 +1,8 @@
-docker run -t -i -v ~/Documents/dev/docker:/mnt/docker omi/ubuntu-dev
+#!/bin/bash
+
+host_shared_dir=/d/shared
+if [ "$(uname)" == "Darwin" ]; then
+    host_shared_dir=~/Documents/dev/docker
+fi
+docker run -t -i -v $host_shared_dir:/mnt/shared omi/ubuntu-dev:latest /bin/zsh
+
